b8ce81d34e51ef521e2696e920818a692371e548
Dinesh Nikam <71932998+dinesh-nikam@users.noreply.github.com>
2024-06-12 21:52:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
- export default {
-  build: {
+   build: {
     rollupOptions: {
       external: ["@google/generative-ai"]
     }
   }
-}
+})
+
