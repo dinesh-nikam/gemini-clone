ec2a9668f3c3bd15971c767ee40c8a0af50468de
Dinesh Nikam <71932998+dinesh-nikam@users.noreply.github.com>
2024-06-14 17:01:45 +0530

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -169,7 +169,7 @@ const Main = () => {
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img onClick={handleSendClick} src={assets.send_icon} alt="" />
+               {input? <img onClick={handleSendClick} src={assets.send_icon} alt="" />:null}  
             </div>
           </div>
           <p className="bottom-info">
