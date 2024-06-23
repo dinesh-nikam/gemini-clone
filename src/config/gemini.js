038e8dd33bb7c7259bd54ad9df2b613761fa4b60
dinesh-nikam <nikamdinesh362@gmail.com>
2024-06-23 22:46:48 +0530

    mobile resonsive

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,55 +1,3 @@
-// // const apiKey ="AIzaSyDANefYjjCeCsXFhVJWVrsN6ZWg87U_qXo";
-
-// /*
-//  * Install the Generative AI SDK
-//  *
-//  * $ npm install @google/generative-ai
-//  *
-//  * See the getting started guide for more information
-//  * https://ai.google.dev/gemini-api/docs/get-started/node
-//  */
-
-// import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
-  
-// const MODEL_NAME ="gemini-1.0-pro";
-// const API_KEY = "AIzaSyDANefYjjCeCsXFhVJWVrsN6ZWg87U_qXo";
-
-//   const apiKey = process.env.GEMINI_API_KEY;
-//   const genAI = new GoogleGenerativeAI(apiKey);
-  
-//   const model = genAI.getGenerativeModel({
-//     model: "gemini-1.5-flash",
-//   });
-  
-//   const generationConfig = {
-//     temperature: 1,
-//     topP: 0.95,
-//     topK: 64,
-//     maxOutputTokens: 8192,
-//     responseMimeType: "text/plain",
-//   };
-  
-//   async function run() {
-//     const chatSession = model.startChat({
-//       generationConfig,
-//    // safetySettings: Adjust safety settings
-//    // See https://ai.google.dev/gemini-api/docs/safety-settings
-//       history: [
-//       ],
-//     });
-  
-//     const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-//     console.log(result.response.text());
-//   }
-  
-//   run();
-
-
-
-
-// node --version # Should be >= 18
-// npm install @google/generative-ai
-
 import {
   GoogleGenerativeAI,
   HarmCategory,
@@ -102,4 +50,4 @@ async function runChat(prompt) {
   return response.text();
 }
 
-export default runChat;
+  export default runChat;
