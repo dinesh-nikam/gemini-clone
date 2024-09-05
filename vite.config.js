c6e4013f9c7f24b81398b9ad4dbfe9e3cc1f3402
dinesh-nikam <nikamdinesh362@gmail.com>
2024-09-05 17:50:48 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '/gemini-clone/',
   plugins: [react()],
-  base: 'gemini-clone',
-   build: {
-    rollupOptions: {
-      external: ["@google/generative-ai"]
-     
-    }
-  }
-   
 })
-
